6d7ce41936b1cec1cb7bbd6f8ddd8f1402932251
Minjae Son <bungabear6422@gmail.com>
2018-02-25 05:45:11 +0900

    버그 수정, 파일, 코드 정리

--- main/router/number.js
+++ main/router/number.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var asc = require('async');
-
+var moment = require('moment');
 // var http = require('http').Server(express);
 // var io = require('socket.io')(http);
 // var http = require('http');
@@ -19,180 +19,210 @@ var pool = mysql.createPool(dbconfig);
 
 var mysqla = require('./mysql.js');
 
+// App이 재시작할때 기 등록된 알림이 있었는지 확인하는 함수.
+function isNumberWait(req, res){
+	var fcmToken = req.body.fcmtoken;
+	var ordernums=[];
+	var cafecode;
+	pool.getConnection(function(err, connection){
+		if(err){
+			res.status(402).send('err');
+		}
+		var query = connection.query('select ordernum, cafecode from number where flag=0 and token=\''+fcmToken + '\'', function(err, rows, fields){
+			if(!err){
+				if(rows.length != 0){
+					for(var i in rows){
+						ordernums.push(rows[i].ordernum);
+					}
+					cafecode = rows[0].cafecode;
+					res.json({"cafecode":cafecode, num:ordernums});
+				}
+				else {
+					// console.log('you are not wait');
+					res.status(400).send('not wait');
+				}
+			}
+			else{
+				console.log('err');
+				res.sendStatus(402);
+			}
+		});
+		connection.release();
+	});
+}
 
 function registerNumber(req, res){
-  var code = req.body.code;
-  var token = req.body.token;
-  var num1 = req.body.num1;
-  var num2 = req.body.num2;
-  var num3 = req.body.num3;
-  var device = req.body.device;
-  var added = [];
-  var nums = [num1];
+	var code = req.body.code;
+	var token = req.body.token;
+	var num1 = req.body.num1;
+	var num2 = req.body.num2;
+	var num3 = req.body.num3;
+	var device = req.body.device;
+	var added = [];
+	var nums = [num1];
 
-  if(num2) nums.push(num2);
-  if(num3) nums.push(num3);
+	if(num2) nums.push(num2);
+	if(num3) nums.push(num3);
 
-  pool.getConnection(function(err, connection){
-    if(err){
-      res.send('err');
-    }
-    else {
-      var tasks = [
-        // function(callback){
-        //   insertDB(connection, 'insert into number set?', {cafecode:code, token:token, ordernum:nums[i], device:device},callback);
-        // }
-      ];
-      nums.map(function(num){tasks.push(function(callback){
-        insertNumber(connection, 'insert into number set?', {cafecode:code, token:token, ordernum:num, device:device},callback);
-      });});
-      // for(var i in nums){
-      //   var num = nums[i];
-      //
-      // }
-      asc.series(tasks, function(err,results){
-        connection.release();
-        while(results.indexOf(0) >-1){
-          results.splice(results.indexOf(0),1);
-        }
-        res.json({"cafecode":code,"token":token, "nums":results});
-        if(results.length == 0) results = "nothing"
-        console.log('[number/registerNumber] cafecode : ' + code + ', ' + results + ' registered.');
-      });
-    }
-  });
+	pool.getConnection(function(err, connection){
+		if(err){
+			res.send('err');
+		}
+		else {
+			var tasks = [
+			];
+			// map은 각각 별도로 호출해 for로 callback을 불렀을때 인덱스 오류를 잡을수 있다.
+			nums.map(function(num){
+				if(num != -1){
+					tasks.push(function(callback){
+						insertNumber(connection, 'insert into number set?', {cafecode:code, token:token, ordernum:num, device:device},callback);
+					});
+				}
+			});
+			asc.series(tasks, function(err,results){
+				connection.release();
+				while(results.indexOf(0) >-1){
+					results.splice(results.indexOf(0),1);
+				}
+				res.json({"cafecode":code,"token":token, "nums":results});
+				if(results.length == 0) results = "nothing"
+				console.log('[number/registerNumber] cafecode : ' + code + ', ' + results + ' registered.');
+			});
+		}
+	});
 }
 
 function insertNumber(connection, sql, data, callback){
-  connection.query(sql,data,
-  function(err, results){
-    var ret;
-    // console.log(nums[i]);
-    if(err){
-      ret = 0;
-    } else{
-      ret = data.ordernum;
-    }
-    callback(null, ret);
-  });
-}
+	connection.query(sql,data,
+		function(err, results){
+			var ret;
+			// console.log(nums[i]);
+			if(err){
+				ret = 0;
+			} else{
+				ret = data.ordernum;
+			}
+			callback(null, ret);
+		});
+	}
 
-// EDMS에서 번호 주는 함수
-function pushNumber(req,res){
-  var num = req.query.num;
-  var code = req.query.code;
-  var cafe = require('../public/cafecode.json');
-  // console.log('[number/getPushNumber/params]' + code + ' ' + num );
+	// EDMS에서 번호 주는 함수
+	function pushNumber(req,res){
+		var num = req.query.num;
+		var code = req.query.code;
+		var cafe = require('../public/cafecode.json');
+		console.log('[number/pushNumber] ' + moment().format('YYYY-MM-DD HH:mm:ss') + ' | cafecode : ' + code + ' : ' + num);
+		// console.log('[number/getPushNumber/params]' + code + ' ' + num );
 
-  const options = {
-    method : 'GET',
-    uri : 'http://inucafeteriaaws.us.to:3829/socket?code='+code+'&num='+num
-  }
+		const options = {
+			method : 'GET',
+			uri : 'http://inucafeteriaaws.us.to:3829/socket?code='+code+'&num='+num
+		}
 
-  request(options,
-    function (error, response, body){
-      if(!error){
-        console.log('[number/pushNumber/request] SUCCESS ' + body);
-        // res.send(body);
-      }
-      else {
-        console.log('[number/pushNumber] Error ' + error);
-      }
-    }
-  );
+		request(options,
+			function (error, response, body){
+				if(!error){
+					// console.log('[number/pushNumber/request] SUCCESS ' + body);
+				}
+				else {
+					console.log('[number/pushNumber] Error ' + error);
+				}
+			}
+		);
 
-    pool.getConnection(function(err, connection){
-      if(err){
-        console.log('[number/pushNumber] DB_Connection_Select_ERROR')
-        res.json({'result':'error'});
-      }
-      var query = connection.query('select token, device from number where ordernum='+ num + ' and cafecode=' + code, function(err, rows, fields){
-        if(!err){
-          switch(rows.length){
-            case 1:
-              connection.release();
-              var token = rows[0].token;
-              var device = rows[0].device;
-              var message;
-              if(device == 'ios'){
-                message = {
-                  to: token,
-                  priority : "high",
-                  notification: {
-                    title: cafe[code-1].name, //title of notification
-                    body: "주문하신 " + num + "번 음식이 완료되었습니다.", //content of the notification
-                    sound: "out.caf",
-                    icon: "ic_launcher", //default notification icon
-                  },
-                  time_to_live : 0
-                };
-              } else{
-                message = {
-                  to: token,
-                  priority : "high",
-                  data: {
-                    title: cafe[code-1].name, //title of notification
-                    body: num, //content of the notification
-                    sound: "default",
-                    icon: "ic_launcher", //default notification icon
-                  },
-                  time_to_live : 0
-                };
-              }
-              fcm.send(message, function(err, response){
-                if (err) {
-                  console.log('[number/pushNumber] FCM_ERROR');
-                  res.json({'result':'ERROR'});
-                  return;
-                }
-                console.log('[number/pushNumber] ' + moment().format('YYYY-MM-DD HH:mm:ss') + ' | ' + restName[code-1] + ' : ' + num + '번 SUCCESS');
-              });
-              break;
-            case 0:
-              // console.log('[number/pushNumber] DB_Multiple_Result : ' + rows.length);
-              break;
-            default:
-              console.log('[number/pushNumber] DB_Multiple_Result : ' + rows.length);
-              break;
-          }
-          res.json({'result':'SUCCESS'});
-        }
-        else {
-          console.log('[number/pushNumber] DB_Connection_ERROR');
-          res.json({'result':'ERROR'});
-        }
-      });
-    }
-  );
+		pool.getConnection(function(err, connection){
+			if(err){
+				console.log('[number/pushNumber] DB_Connection_Select_ERROR')
+				res.json({'result':'error'});
+				return;
+			}
+			var query = connection.query('select token, device from number where ordernum=? and cafecode=? and flag=0', [num, code], function(err, rows, fields){
+				if(!err){
+					switch(rows.length){
+						case 1:
+						var token = rows[0].token;
+						var device = rows[0].device;
+						connection.query('update number set flag=1 where token=? and ordernum=? and cafecode=? ', [token, num, code], function(err, rows, fields){
+							if(err){
+								console.log('[number/pushNumber] DB_UPDATE_ERR ' + err);
+							}
+						});
+						connection.release();
+						var message;
+						if(device == 'ios'){
+							message = {
+								to: token,
+								priority : "high",
+								notification: {
+									title: cafe[code-1].name, //title of notification
+									body: "주문하신 " + num + "번 음식이 완료되었습니다.", //content of the notification
+									sound: "out.caf",
+									icon: "ic_launcher", //default notification icon
+								},
+								time_to_live : 0
+							};
+						} else{
+							message = {
+								to: token,
+								priority : "high",
+								data: {
+									title: cafe[code-1].name, //title of notification
+									body: num, //content of the notification
+									sound: "default",
+									icon: "ic_launcher", //default notification icon
+								},
+								time_to_live : 0
+							};
+						}
+						fcm.send(message, function(err, response){
+							if (err) {
+								console.log('[number/pushNumber] FCM_ERROR');
+								return res.json({'result':'ERROR'});
+							}
+						});
+						break;
+						case 0:
+							// console.log('[number/pushNumber] Not_Exist');
+							return res.json({'result':'SUCCESS'});
+						break;
+					}
+
+					res.json({'result':'SUCCESS'});
+				}
+				else {
+					console.log('[number/pushNumber] DB_Connection_ERROR');
+				}
+			});
+		}
+	);
 }
 
 function resetNumber(req, res){
-  var fcmtoken = req.body.fcmtoken;
-  pool.getConnection(function(err, connection){
-    if(err){
-      res.send('err');
-    }
-    else{
-      connection.query('delete from number where token=\'' + fcmtoken + '\'' ,
-      function(err, results){
-        connection.release();
-        if(err){
-          if(typeof callback === 'fucntion') callback('err');
-        } else{
-          if(typeof callback === 'function') callback('suc');
-        }
-      }
-    );}
-    if(fcmtoken != null){
-      console.log('[NUMBER/RESETNUMBER] SUCCESS');
-      res.json({"result":"success"});
-    } else{
-      console.log('[NUMBER/RESETNUMBER] ERROR');
-      res.json({"result":"error"});
-    }
-  });
+	var fcmtoken = req.body.fcmtoken;
+	if(!fcmtoken){
+		return res.status(400).sned('err');
+	}
+	pool.getConnection(function(err, connection){
+		if(err){
+			res.status(402).send('err');
+		}
+		else{
+			connection.query('delete from number where token=\'' + fcmtoken + '\'' ,
+			function(err, results){
+				connection.release();
+				if(err){
+					console.log('[number/resetNumber] DB_QUERY_ERROR :' + fcmtoken)
+					return res.status(402).send('err');
+				} else{
+					console.log('[NUMBER/RESETNUMBER] SUCCESS : ' + fcmtoken);
+					return res.json({"result":"success"});
+				}
+			}
+		);}
+	});
 }
 
+module.exports.isNumberWait = isNumberWait;
 module.exports.pushNumber = pushNumber;
 module.exports.registerNumber = registerNumber;
 module.exports.resetNumber = resetNumber;
