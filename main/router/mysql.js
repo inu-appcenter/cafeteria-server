6646362e734ddf970ff1a26422a5385810241ce5
Minjae Son <bungabear6422@gmail.com>
2018-03-08 13:18:44 +0900

    logger 적용, 시간대별 할인 구분, 바코트 재확인시 딜레이 적용

--- main/router/mysql.js
+++ main/router/mysql.js
@@ -1,12 +1,14 @@
 // copyright(c) 2017 All rights reserved by jaemoon(jjaemny@naver.com) 201201646 정보통신공학과 신재문
 
-var mysql = require('mysql');
-var dbconfig = require('../config.js').MYSQL_CONFIG;
-var pool = mysql.createPool(dbconfig);
+const mysql = require('mysql');
+const dbconfig = require('../config.js').MYSQL_CONFIG;
+const pool = mysql.createPool(dbconfig);
+const logger = require('./logger.js');
 
 function setAutoLogin(token, sno, device){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, setAutoLogin);
       res.send('err');
     }
     else{
@@ -14,9 +16,10 @@ function setAutoLogin(token, sno, device){
       function(err, results){
         connection.release();
         if(err){
-          if(typeof callback === 'fucntion') callback('err');
+          logger('error', err, setAutoLogin);
+          // if(typeof callback === 'fucntion') callback('err');
         } else{
-          if(typeof callback === 'function') callback('suc');
+          // if(typeof callback === 'function') callback('suc');
         }
       }
     );}
@@ -27,6 +30,7 @@ function setAutoLogin(token, sno, device){
 function checkTokenDup(token){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, checkTokenDup);
       res.send('err');
     }
     var query = connection.query('select token from autologin where token=\'' + token + '\'', function(err, rows, fields){
@@ -40,7 +44,7 @@ function checkTokenDup(token){
         }
       }
       else{
-        console.log('[mysql/checkTokenDup] QUERY_ERROR' + err);
+        logger('info','QUERY_ERROR' + err, checkTokenDup);
         ret = null;
       }
       connection.release();
@@ -53,6 +57,7 @@ function checkTokenDup(token){
 function releaseAutoLogin(token){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, releaseAutoLogin);
       res.send('err');
     }
     else{
@@ -60,18 +65,20 @@ function releaseAutoLogin(token){
       function(err, results){
         connection.release();
         if(err){
-          if(typeof callback === 'fucntion') callback('err');
+          logger('error', err, releaseAutoLogin);
+          // if(typeof callback === 'fucntion') callback('err');
         } else{
-          if(typeof callback === 'function') callback('suc');
+          // if(typeof callback === 'function') callback('suc');
         }
       }
-    )}
-  })
+    );}
+  });
 }
 
 function releaseAutoLoginBySno(sno){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, releaseAutoLoginBySno);
       res.send('err');
     }
     else{
@@ -79,18 +86,20 @@ function releaseAutoLoginBySno(sno){
       function(err, results){
         connection.release();
         if(err){
-          if(typeof callback === 'fucntion') callback('err');
+          logger('error', err, releaseAutoLoginBySno);
+          // if(typeof callback === 'fucntion') callback('err');
         } else{
-          if(typeof callback === 'function') callback('suc');
+          // if(typeof callback === 'function') callback('suc');
         }
       }
-    )}
-  })
+    );}
+  });
 }
 
 function deleteBarcode(barcode){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, deleteBarcode);
       res.send('err');
     }
     else{
@@ -98,18 +107,20 @@ function deleteBarcode(barcode){
       function(err, results){
         connection.release();
         if(err){
-          if(typeof callback === 'fucntion') callback('err');
+          logger('error', err, deleteBarcode);
+          // if(typeof callback === 'fucntion') callback('err');
         } else{
-          if(typeof callback === 'function') callback('suc');
+          // if(typeof callback === 'function') callback('suc');
         }
       }
-    )}
-  })
+    );}
+  });
 }
 
 function activateBarcode(active, barcode){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, activateBarcode);
       res.send('err');
     }
     else{
@@ -117,6 +128,7 @@ function activateBarcode(active, barcode){
       function(err, results){
         connection.release();
         if(err){
+          logger('error', err, activateBarcode);
           // if(typeof callback === 'fucntion') callback('err');
         } else{
           // if(typeof callback === 'function') callback('suc');
@@ -130,49 +142,55 @@ function activateBarcode(active, barcode){
 function addBarcode(barcode){
   pool.getConnection(function(err, connection){
     if(err){
-      res.send('err');
+      logger('error', err, addBarcode);
+      res.sendStatus(404);
+      return false;
     }
     else{
       connection.query('insert into barcode set?',{"barcode":barcode},
       function(err, results){
         connection.release();
         if(err){
-          if(typeof callback === 'fucntion') callback('err');
+          return false;
         } else{
-          if(typeof callback === 'function') callback('suc');
+          return true;
         }
       }
-    )}
-  })
+    );}
+  });
 }
 
 function checkBarcode(barcode){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, checkBarcode);
       res.send('err');
     }
     var query = connection.query('select barcode from barcode where barcode=\''+barcode + '\'', function(err, rows, fields){
+      connection.release();
       if(!err){
         if(rows.length != 0){
+          return true;
           //console.log('[mysql/checkBarcode] ' + barcode + ' is already exist');
         }
         else{
           //console.log('[mysql/checkBarcode] ' + barcode + ' SUCCESS');
-          addBarcode(barcode);
+          return addBarcode(barcode);
         }
       }
       else{
-        console.log('[mysql/checkBarcode] ' + barcode + ' QUERY_ERROR');
+        logger('info','barcode : ' + barcode + ' QUERY_ERROR', checkBarcode);
         res.sendStatus(404);
+        return false;
       }
     });
-    connection.release();
   });
 }
 
 function putError(sno,title,msg,device,service){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, putError);
       res.send('err');
     }
     else{
@@ -180,9 +198,10 @@ function putError(sno,title,msg,device,service){
       function(err, results){
         connection.release();
         if(err){
-          if(typeof callback === 'fucntion') callback('err');
+          logger('error',err, putError);
+          // if(typeof callback === 'fucntion') callback('err');
         } else{
-          if(typeof callback === 'function') callback('suc');
+          // if(typeof callback === 'function') callback('suc');
         }
       }
     );}
@@ -192,6 +211,7 @@ function putError(sno,title,msg,device,service){
 function getError(callback){
   pool.getConnection(function(err, connection){
     if(err){
+      logger('error', err, getError);
       res.send('err');
     }
     else{
@@ -199,6 +219,7 @@ function getError(callback){
       function(err, rows, fields){
         connection.release();
         if(err){
+          logger('error', err, getError);
           // if(typeof callback === 'fucntion') callback('err');
         } else{
           // if(typeof callback === 'function') callback('suc');
@@ -208,7 +229,94 @@ function getError(callback){
     );}
   });
 }
+function updateBarcodeCheckTime(barcode){
+  pool.getConnection(function(err, connection){
+    if(err){
+      logger('error', err, updateBarcodeCheckTime);
+      return err;
+    }
+    else{
+      connection.query('UPDATE barcode set lastchecktime = CURRENT_TIMESTAMP where barcode = ' + barcode,
+      function(err, rows, fields){
+        connection.release();
+        if(err){
+          logger('error', err, updateBarcodeCheckTime);
+        } else{
+          // console.log(rows);
+        }
+      }
+    );}
+  });
+}
 
+function getPaymentCount(){
+  pool.getConnection(function(err, connection){
+    if(err){
+      logger('error', err, getPaymentCount);
+      return err;
+    }
+    else{
+      connection.query('SELECT cafecode, count(*) as count FROM `payment` GROUP BY cafecode',
+      function(err, rows, fields){
+        connection.release();
+        if(err){
+          logger('error', err, getPaymentCount);
+          return err;
+        } else{
+          // console.log(rows);
+          return rows;
+        }
+      }
+    );}
+  });
+}
+
+function getNumberCount(){
+  pool.getConnection(function(err, connection){
+    if(err){
+      logger('error', err, getNumberCount);
+      return err;
+    }
+    else{
+      connection.query('SELECT flag, count(*) as count FROM `number` GROUP BY flag',
+      function(err, rows, fields){
+        connection.release();
+        if(err){
+          logger('error', err, getNumberCount);
+          return err;
+        } else{
+          // logger('info', rows, getNumberCount);
+          // console.log(rows);
+          return rows;
+        }
+      }
+    );}
+  });
+}
+
+function customQuery(sql, cb){
+  pool.getConnection(function(err, connection){
+    if(err){
+      logger('error', err, customQuery);
+      return err;
+    }
+    else{
+      connection.query(sql,
+      function(err, rows, fields){
+        connection.release();
+        if(err){
+          logger('error', err, customQuery);
+          return err;
+        } else{
+          cb(rows);
+          return rows;
+        }
+      }
+    );}
+  });
+}
+module.exports.updateBarcodeCheckTime = updateBarcodeCheckTime;
+module.exports.customQuery = customQuery;
 module.exports.setAutoLogin = setAutoLogin;
 module.exports.releaseAutoLogin = releaseAutoLogin;
 module.exports.releaseAutoLoginBySno = releaseAutoLoginBySno;
@@ -221,3 +329,5 @@ module.exports.checkBarcode = checkBarcode;
 
 module.exports.putError = putError;
 module.exports.getError = getError;
+module.exports.getPaymentCount = getPaymentCount;
+module.exports.getNumberCount = getNumberCount;
