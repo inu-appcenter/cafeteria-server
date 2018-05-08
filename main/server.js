4e5d3235d865c5780bf2c33df28e4d4aa9ca8b9f
Minjae Son <bungabear6422@gmail.com>
2018-05-08 11:09:52 +0900

    로그 수정 및 일부 코드 변경
    pm2로 변경함에 따라 기존 로그 호스팅 제외, 테스트용 kill 코드 추가
    Android용 식단정보 path 추가

--- main/server.js
+++ main/server.js
@@ -36,7 +36,10 @@ if (cluster.isMaster) {
 	// TODO DB 일별 초기화.
 	const bot = require('./router/telegrambot.js');
 	schedule.scheduleJob('0 7 * * *', food.getFoodPlans);
-	schedule.scheduleJob('0 0 * * *', console.clear());
+	// Payment DB를 log/payment에 기록하고 truncate
+	schedule.scheduleJob('42 22 * * *', logger.storePaymentHistory);
+	// std.log와 err.log를 /log 폴더에 복사해놓고 truncate
+	// schedule.scheduleJob('50 23 * * *', logger.clearCurrentLog);
 	for (var i = 0; i < 1; i++) {
 		var child = cluster.fork();
 		logger('info','worker '+child.process.pid+' born at init.');
@@ -45,8 +48,7 @@ if (cluster.isMaster) {
 		var worker = cluster.fork();
 		var newPID = worker.process.pid;
 		var oldPID = deadWorker.process.pid;
-		logger('error','worker '+oldPID+' died.');
-		logger('error','worker '+newPID+' born.');
+		logger('error','worker '+oldPID+' die, ' + newPID + 'born');
 	});
 } else {
 
@@ -84,8 +86,6 @@ if (cluster.isMaster) {
 	app.use('/log', express.static(__dirname + '/log'));
 	app.use('/logs', serveIndex(__dirname + '/public/log'));
 	app.use('/logs', express.static(__dirname + '/public/log'));
-	// app.use(timeout(6*1000));
-	// app.use(haltOnTimedout);
 
 	// app.use(morgan('[:current_time] IP:remote-addr Method:method Status:status Respons-time :response-time ms'),
 	// {skip:function (req,res){return req.url == '/activeBarcode' || '/socket' || '/errormsg' || '/food' || '/ads' || '/' || '/js'}, stream:accessLogStream});
@@ -93,28 +93,34 @@ if (cluster.isMaster) {
 	app.set('view engine', 'jade');
 	app.use(function(req, res, next){
 		res.setTimeout(3*1000, function(){
-			logger('error-only', 'response time out : ' + req.originalUrl + '\n' + req);
-			res.status(200).json({message : "SUCCESS", activated:0});
-			// process.exit();
+			logger('error', 'response time out : ' + req.originalUrl + '\n' + JSON.stringify(req.body || '', null, ' '));
+			if(!res.headerSent) res.status(200).json({message : "SUCCESS", activated:0});
+			process.exit();
 		});
 		next();
 	});
 
 	app.use(function(err, req, res, next){
-		logger('error', 'errorHandler : ' + req.originalUrl + '\n' + req + '\n' + err);
-		res.status(200).json({message : "SUCCESS", activated:0});
+		if(req.originalUrl == '/activeBarcode'){
+			logger('error-only', 'errorHandler : ' + req.originalUrl + '\n' + JSON.stringify(req.body || '', null, ' ') + '\n' + err);
+		}
+		else {
+			logger('error', 'errorHandler : ' + req.originalUrl + '\n' + JSON.stringify(req.body || '', null, ' ') + '\n' + err);
+		}
+		if(!res.headerSent) res.status(200).json({message : "SUCCESS", activated:0});
 		process.exit();
 	});
 
-	app.get('/std.log', (req, res) => {	// read all lines:
-		logger.readReverse('./std.log', (str)=>{res.send('<pre>위가 최신\n'+str+'</pre>')});
-	});
-	app.get('/err.log', (req, res) => {	// read all lines:
-		logger.readReverse('./err.log', (str)=>{res.send('<pre>위가 최신\n'+str+'</pre>')});
-	});
+	// app.get('/std.log', (req, res) => {	// read all lines:
+	// logger.readReverse('./std.text', (str)=>{res.send('<pre>위가 최신\n'+str+'</pre>')});
+	// });
+	// app.get('/err.log', (req, res) => {	// read all lines:
+	// logger.readReverse('./err.text', (str)=>{res.send('<pre>위가 최신\n'+str+'</pre>')});
+	// });
 
 	app.get('/ads', (req, res) => {res.render('ads');});
 	app.get('/store', (req, res) => {res.render('store');});
+	app.get('/food/:date/:type', food.food);
 	app.get('/food/:date', food.food);
 	app.post('/adSet', ad.upload().single('userfile'), ad.adSet);
 
@@ -133,8 +139,12 @@ if (cluster.isMaster) {
 
 	app.post('/errormsg',etc.postErrorMessage);
 	app.get('/errormsg', etc.getErrorMessage);
+	app.get('/kill', function(req,res){
+		res.send("kill cluster");
+		process.exit();
+	});
 
 	server.listen(port);
 	// 클라이언트가 응답을 받지않고 연결을 끊으면 서버가 2분간 대기하게 되므로, 적절히 짧은 시간을 주어야한다.
-	server.timeout = 2*1000;
+	// server.timeout = 2*1000;
 }
