c078135754f4b635565f6ee09aa2ab21c8e4e836
Minjae Son <bungabear6422@gmail.com>
2018-06-30 21:33:51 +0900

    식단 서버 대응 코드 재적용, etc route에 return 추가

--- main/router/food.js
+++ main/router/food.js
@@ -4,11 +4,12 @@ const cafecode = require('../public/cafecode.json');
 const util = require('util');
 const path = require('path');
 const moment = require('moment');
-const logger = require('./logger.js');
+// const logger = require('./logger.js');
 
-const time = ["", "조식", "중식", "석식"];
+const time = ["", "아침", "점심", "저녁"];
 
 var makeResultName = function(no){
+  // 'foodMenuType[no]Result'
   return util.format('foodMenuType%sResult',no);
 };
 
@@ -28,92 +29,157 @@ var makeConerName = function(code, no, time){
 };
 
 function food(req, res){
-  var date = req.params.date;
-  // console.log(date);
-  fs.exists(path.join(__dirname, '../public/food', date) + date, (exists) => {
+  let date = req.params.date;
+  let type = req.params.type;
+  let url = req.originalUrl;
+  let pathname = url.split('/')[1];
+  let filepath = '../public/'
+
+  // console.log(url.split('/'));
+  if(type == 'android' || pathname == 'foodplan'){
+    filepath += 'android';
+  }
+  filepath += 'food'
+
+  fs.exists(path.join(__dirname, filepath, date + '.json'), (exists) => {
     if(exists){
-      res.json(require('../public/food/'+date));
+      res.json(require(filepath + '/' + date + '.json'));
+      return
     }
     else {
-      res.sendStatus(400);
+      res.sensStatus(400);
+      return
     }
+  });
+
+}
+
+// request wrapper function
+function doRequest(options) {
+  return new Promise(function (resolve, reject) {
+    request(options, function (error, res, body) {
+      if (!error && res.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
   });
 }
 
-function getFoodPlan(date){
-  var options = {
+async function getFoodPlan(date, mode){
+  let requset_options_smartcampus = {
     uri : 'https://sc.inu.ac.kr/inumportal/main/info/life/foodmenuSearch?stdDate=' + date,
     method : 'GET'
   };
 
-  request(options, function(err, result){
-    if(!err){
-      var menus = [];
-      var json = result.body;
-      json = JSON.parse(json);
-      // cafecode의 식당만 사용함.
-      // cafecode의 menu값은 request해오는 결과의 번호.
-      // type1은 코너 (1~5, A/B)
-      // type2는 조/중/석식
-      json.weekDay = json.dayOfWeekString;
-      delete json.dayOfWeekString;
-      delete json.afterDay;
-      delete json.beforeDay;
-      for(let i in cafecode){
-        let menuNum = cafecode[i].menu;
-        if(menuNum != -1){
-          let conercheck = true;
-          json[cafecode[i].no + ''] = json[makeResultName(menuNum)];
-          delete json[makeResultName(menuNum)];
-          menus = json[cafecode[i].no + ''];
-          for(let j in menus){
-            let menu = menus[j];
-            if(menu.MENU == null){
-              menu.MENU = "";
-            }
-            else {
-              menu.MENU = menu.MENU.replace(/<span.*?>(.*?)<\/span>/gi,'$1');
-              menu.MENU = menu.MENU.replace(/<hr(.*?)\/>/gi,'');
-              menu.MENU = menu.MENU.replace(/amp;/gi,'');
-            }
-            delete menu.FOODMENU_TYPE;
-            delete menu.STD_DATE;
-            delete menu.FOODMENU_TYPE;
-            delete menu.STD_DATE;
-            let k = j*1+1;
-            // console.log(menus[j]);
-            if(menus[k]){
-              if(menu.TYPE1 != menus[k].TYPE1) {
-                conercheck = false;
-              }
-            }
-            menu.TYPE2 = time[menu.TYPE2];
-          }
-          for(let j in menus){
-            let menu = menus[j];
-            if(conercheck){
-              menu.TITLE = menu.TYPE2;
-            }
-            else {
-              menu.TITLE = makeConerName(i, menu.TYPE1, menu.TYPE2);
-            }
-            delete menu.TYPE1;
-            delete menu.TYPE2;
-            menu.order = j;
-          }
-        }
+  let request_options_tmp_menu_server = {
+    uri : 'http://inucafeteriaaws.us.to:3829/test.json',
+    method : 'GET'
+  }
+
+  // ---------------스캠 데이터 파싱 시작
+  let smartcampus_food = await doRequest(requset_options_smartcampus);
+
+  let menus = [];
+  let json = smartcampus_food;
+  json = JSON.parse(json);
+  // cafecode의 식당만 사용함.
+  // cafecode의 menu값은 request해오는 결과의 번호.
+  // type1은 코너 (1~5, A/B)
+  // type2는 조/중/석식
+  json.weekDay = json.dayOfWeekString;
+
+  // 불필요한 속성 삭제
+  delete json.dayOfWeekString;
+  delete json.afterDay;
+  delete json.beforeDay;
+  for(let i in cafecode){
+    let menuNum = cafecode[i].menu;
+    if(menuNum != -1 && json[[makeResultName(menuNum)]] != undefined ){
+      let conercheck = true;
+      // mode가 0이면 iOS, 1이면 Android용.
+      // iOS는 json object이름으로 숫자만 주는데, Android는 Gson을 쓰느라 java 규칙에 어긋나 cafe+숫자 사용.
+      //
+      if(mode == 0){
+        json[cafecode[i].no + ''] = json[makeResultName(menuNum)];
+        menus = json[cafecode[i].no + ''];
       }
-      fs.writeFile(path.join(__dirname, '../public/food', date), JSON.stringify(json,null,'\t'), function(err){
-        if(!err){
-          logger('info','식단 저장 ' + date, getFoodPlan);
-          logger('telegram','식단 저장 ' + date, getFoodPlan);
+      else {
+        json['cafe' + cafecode[i].no] = json[makeResultName(menuNum)];
+        menus = json['cafe' + cafecode[i].no];
+      }
+      delete json[makeResultName(menuNum)];
+      for(let j in menus){
+        let menu = menus[j];
+        // null, 태그 제거
+        if(menu.MENU == null){
+          menu.MENU = "";
         }
         else {
-          logger('info', err, getFoodPlan);
+          menu.MENU = menu.MENU.replace(/<span.*?>(.*?)<\/span>/gi,'$1');
+          menu.MENU = menu.MENU.replace(/<hr(.*?)\/>/gi,'');
+          menu.MENU = menu.MENU.replace(/amp;/gi,'');
         }
-      });
-      // console.log(json);
-      // return json;
+        delete menu.FOODMENU_TYPE;
+        delete menu.STD_DATE;
+        delete menu.FOODMENU_TYPE;
+        delete menu.STD_DATE;
+        let k = j*1+1;
+        if(menus[k]){
+          if(menu.TYPE1 != menus[k].TYPE1) {
+            conercheck = false;
+          }
+        }
+        menu.TYPE2 = time[menu.TYPE2];
+
+        if(conercheck){
+          menu.TITLE = menu.TYPE2;
+        }
+        else {
+          menu.TITLE = makeConerName(i, menu.TYPE1, menu.TYPE2);
+        }
+        delete menu.TYPE1;
+        delete menu.TYPE2;
+        menu.order = j;
+      }
+
+      // 식단 Title 생성
+      // for(let j in menus){
+      //   let menu = menus[j];
+      //
+      // }
+    } // menu num check if end
+  } // cafecode for end
+
+
+
+  // ---------------스캠 데이터 파싱 종료
+
+  // ---------------메뉴 서버 데이터 추가
+  // let menu_server_food = await doRequest(request_options_tmp_menu_server);
+  // let parsed = JSON.parse(menu_server_food)
+  // let number = 0;
+  // let i = 11;
+  // if(mode == 0){
+  //   json[i + ''] = parsed;
+  // }
+  // else {
+  //   json['cafe' + i] = parsed;
+  // }
+
+
+  // 파일로 저장
+  var dir = '../public/';
+  if(mode == 0){
+    dir += 'food';
+  }
+  else {
+    dir += 'androidfood';
+  }
+  fs.writeFile(path.join(__dirname, dir, date + '.json'), JSON.stringify(json,null,'\t'), function(err){
+    if(err){
+      logger('error', err, getFoodPlan);
     }
   });
 }
@@ -121,15 +187,17 @@ function getFoodPlan(date){
 // getFoodPlan('20180101')
 
 function getFoodPlans(){
-	var now = Date.now();
-	var aDay = 86400000;
+  var now = Date.now();
+  var aDay = 86400000;
   var someday;
-	for(var i = 0; i < 7; i++){
-		someday = now + (aDay * i);
+  for(var i = 0; i < 7; i++){
+    someday = now + (aDay * i);
     // console.log(now + (aDay * i));
-		var date = moment(someday).format('YYYYMMDD');
-		getFoodPlan(date);
-	}
+    var date = moment(someday).format('YYYYMMDD');
+    // TODO android, iOS 분리코드
+    getFoodPlan(date, 0);
+    getFoodPlan(date, 1);
+  }
 }
 
 // getFoodPlans();
