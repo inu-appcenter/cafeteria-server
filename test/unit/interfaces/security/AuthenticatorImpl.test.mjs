d658d3470d4173df5c80975e891d1619224e93c8
potados99 <potados99@gmail.com>
2020-03-21 17:49:47 +0900

    Add test for AuthenticatorImpl.

---
+++ test/unit/interfaces/security/AuthenticatorImpl.test.mjs
@@ -0,0 +1,37 @@
+/**
+ * This file is part of INU Cafeteria.
+ *
+ * Copyright (C) 2020 INU Global App Center <potados99@gmail.com>
+ *
+ * INU Cafeteria is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INU Cafeteria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import resolve, {init} from '../../../../lib/common/di/resolve';
+import testModules from '../../../testModules';
+
+import Authenticator from '../../../../lib/domain/security/Authenticator';
+
+beforeEach(async () => {
+  await init(testModules, true);
+});
+
+describe('# authenticateJwt', () => {
+  it('should say invalid to non-existing id', async () => {
+    expect(await resolve(Authenticator).authenticateJwt({id: 8935872235})).toEqual({isValid: false});
+  });
+
+  it('should say valid', async () => {
+    expect(await resolve(Authenticator).authenticateJwt({id: 201701562})).toEqual({isValid: true});
+  });
+});
