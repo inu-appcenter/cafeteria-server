3e1937afc4e97f34488986af5568aaf456953166
potados99 <potados99@gmail.com>
2020-11-28 08:51:16 +0900

    Update sequelize.

--- test/unit/infrastructure/database/sequelizeMock.mjs
+++ test/unit/infrastructure/database/sequelizeMock.mjs
@@ -17,61 +17,9 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import config from '../../../../config';
+import {createSequelizeInstance, registerModels} from '../../../../lib/infrastructure/database/configurations/sequelizeHelper';
 
-import SequelizeMock from 'sequelize-mock';
-
-import CafeteriaValidationParams from '../../../../lib/infrastructure/database/models/CafeteriaValidationParams';
-import DiscountTransaction from '../../../../lib/infrastructure/database/models/DiscountTransaction';
-import UserDiscountStatus from '../../../../lib/infrastructure/database/models/UserDiscountStatus';
-import Cafeteria from '../../../../lib/infrastructure/database/models/Cafeteria';
-import Corner from '../../../../lib/infrastructure/database/models/Corner';
-import User from '../../../../lib/infrastructure/database/models/User';
-import Feedback from '../../../../lib/infrastructure/database/models/Feedback';
-import Notification from '../../../../lib/infrastructure/database/models/FeedbackReply';
-import Notice from '../../../../lib/infrastructure/database/models/Notice';
-import CafeteriaDiscountRule from '../../../../lib/infrastructure/database/models/CafeteriaDiscountRule';
-import ParseRegex from '../../../../lib/infrastructure/database/models/ParseRegex';
-
-import seq from 'sequelize';
-import TransactionHistory from '../../../../lib/infrastructure/database/models/TransactionHistory';
-const {DataTypes} = seq;
-
-const seqConfig = Object.assign({}, config.sequelize);
-
-const sequelize = new SequelizeMock(
-  config.sequelize.database,
-  config.sequelize.username,
-  config.sequelize.password,
-  seqConfig,
-);
-
-const models = {
-  CafeteriaValidationParams: CafeteriaValidationParams(sequelize, DataTypes),
-  DiscountTransaction: DiscountTransaction(sequelize, DataTypes),
-  UserDiscountStatus: UserDiscountStatus(sequelize, DataTypes),
-  CafeteriaDiscountRule: CafeteriaDiscountRule(sequelize, DataTypes),
-
-  Cafeteria: Cafeteria(sequelize, DataTypes),
-  Corner: Corner(sequelize, DataTypes),
-  User: User(sequelize, DataTypes),
-
-  Feedback: Feedback(sequelize, DataTypes),
-  Notification: Notification(sequelize, DataTypes),
-  Notice: Notice(sequelize, DataTypes),
-
-  ParseRegex: ParseRegex(sequelize, DataTypes),
-  TransactionHistory: TransactionHistory(sequelize, DataTypes),
-};
-
-// Do associate.
-Object.keys(models).forEach((modelName) => {
-  if (models[modelName].associate) {
-    models[modelName].associate(models);
-  }
-
-  // sequelize-mock has no findByPk. Therefore we add a proxy.
-  models[modelName].findByPk = (key) => models[modelName].findById(key);
-});
+const sequelize = createSequelizeInstance({mock: true});
+registerModels(sequelize);
 
 export default sequelize;
