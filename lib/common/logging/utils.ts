770292802f77d266c4ca0512fdff233d2e9e84f3
potados99 <potados99@gmail.com>
2021-08-13 20:05:38 +0900

    chore: 로깅할 때 스텍트레이스에서 source map support 사용!

--- lib/common/logging/utils.ts
+++ lib/common/logging/utils.ts
@@ -17,27 +17,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import stackTrace from 'stack-trace';
 import path from 'path';
-
-/**
- * This file is part of INU Cafeteria.
- *
- * Copyright (C) 2020 INU Global App Center <potados99@gmail.com>
- *
- * INU Cafeteria is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * INU Cafeteria is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+import {wrapCallSite} from 'source-map-support';
 
 export function stringify(object: any) {
   if (object.stack) {
@@ -56,7 +37,13 @@ export function stringify(object: any) {
 }
 
 export function formatLog(message: any, showCaller = true) {
-  const caller = stackTrace.get()[2]; /* to get a real caller */
+  // source-map-support 의 매핑을 활용합니다.
+  Error.prepareStackTrace = (_, stack) => stack.map((s) => wrapCallSite(s));
+  const error = new Error();
+  const stack = error.stack as unknown as StackFrame[];
+  Error.prepareStackTrace = undefined;
+
+  const caller = stack[2]; /* 진짜 caller는 2에 있음! */
   const payload = stringify(message);
 
   if (showCaller) {
@@ -69,4 +56,14 @@ export function formatLog(message: any, showCaller = true) {
   } else {
     return `${payload}`;
   }
+}
+
+export interface StackFrame {
+  getTypeName(): string;
+  getFunctionName(): string;
+  getMethodName(): string;
+  getFileName(): string;
+  getLineNumber(): number;
+  getColumnNumber(): number;
+  isNative(): boolean;
 }
