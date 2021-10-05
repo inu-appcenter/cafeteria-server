649094b0616c57ed6d997cb6d414e36f372f1015
potados99 <potados99@gmail.com>
2021-10-05 09:44:11 +0900

    fix: 휴대전화 로그인 라우트 1분에 20분으로 제한 변경

--- lib/infrastructure/webserver/libs/route.ts
+++ lib/infrastructure/webserver/libs/route.ts
@@ -36,8 +36,8 @@ export function defineRoute<TParams = any, TQuery = any, TBody = any>(
 }
 
 export const apiLimiter = rateLimit({
-  windowMs: 1000 * 60 * 5, // 5분에
-  max: 15, // 15번
+  windowMs: 1000 * 60 * 1, // 1분에
+  max: 20, // 20번
   handler(req, res, next) {
     res.status(429).json({
       statusCode: 429,
