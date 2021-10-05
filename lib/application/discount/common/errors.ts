49b2f523257a175c8e8dfc49abd5ffc95b552cb6
potados99 <potados99@gmail.com>
2021-10-05 11:07:33 +0900

    refactor: 응답 메시지에서 개행문자 제거

--- lib/application/discount/common/errors.ts
+++ lib/application/discount/common/errors.ts
@@ -21,7 +21,7 @@ import BadRequest from '../../../common/errors/http/BadRequest';
 
 export const RequestMalformed = BadRequest.of(
   'request_malformed',
-  '잘못된 형식의 요청입니다.\n파라미터를 다시 확인하고 요청해주세요.'
+  '잘못된 형식의 요청입니다. 파라미터를 다시 확인하고 요청해주세요.'
 );
 
 export const DiscountNotSupportedHere = BadRequest.of(
@@ -31,25 +31,25 @@ export const DiscountNotSupportedHere = BadRequest.of(
 
 export const DiscountNotAvailableNow = BadRequest.of(
   'not_available_now',
-  '지금은 식당이 할인을 제공하는 시간대가 아닙니다.\n다음에 다시 시도해 주세요.'
+  '지금은 식당이 할인을 제공하는 시간대가 아닙니다. 다음에 다시 시도해 주세요.'
 );
 
 export const UserNotIdentified = BadRequest.of(
   'user_not_identified',
-  '사용자를 확인할 수 없습니다.\n앱에 로그인되어있는지 확인해주세요.'
+  '사용자를 확인할 수 없습니다. 앱에 로그인되어있는지 확인해주세요.'
 );
 
 export const BarcodeNotActivated = BadRequest.of(
   'barcode_not_activated',
-  '바코드가 활성화되어있지 않습니다.\n앱이 켜져있는지 확인해주세요.'
+  '바코드가 활성화되어있지 않습니다. 앱이 켜져있는지 확인해주세요.'
 );
 
 export const BarcodeUsedRecently = BadRequest.of(
   'barcode_used_recently',
-  '바코드를 너무 빠르게 태그했습니다.\n잠시 후에 다시 시도해 주세요.'
+  '바코드를 너무 빠르게 태그했습니다. 잠시 후에 다시 시도해 주세요.'
 );
 
 export const DiscountAlreadyMadeHereToday = BadRequest.of(
   'discount_already_made_here_today',
-  '오늘 이 식당에서 이미 할인을 제공받았습니다.\n할인은 식당별로 하루에 한 번씩 제공받으실 수 있습니다.'
+  '오늘 이 식당에서 이미 할인을 제공받았습니다. 할인은 식당별로 하루에 한 번씩 제공받으실 수 있습니다.'
 );
