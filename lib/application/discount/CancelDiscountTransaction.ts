dad370779e579d2c4f2eca05322ad16d1be5a46b
potados99 <potados99@gmail.com>
2022-01-24 16:37:17 +0900

    chore: Copyright 업데이트

--- lib/application/discount/CancelDiscountTransaction.ts
+++ lib/application/discount/CancelDiscountTransaction.ts
@@ -1,7 +1,7 @@
 /**
  * This file is part of INU Cafeteria.
  *
- * Copyright (C) 2021 INU Global App Center <potados99@gmail.com>
+ * Copyright 2021 INU Global App Center <potados99@gmail.com>
  *
  * INU Cafeteria is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
