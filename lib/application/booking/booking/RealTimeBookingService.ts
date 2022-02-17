2d3c080652c4cc91238bcfd2f1c8076b92550504
potados99 <potados99@gmail.com>
2022-02-17 23:48:53 +0900

    feat: 실시간 예약내역 서비스 추가

---
+++ lib/application/booking/booking/RealTimeBookingService.ts
@@ -0,0 +1,53 @@
+/**
+ * This file is part of INU Cafeteria.
+ *
+ * Copyright 2022 INU Global App Center <potados99@gmail.com>
+ *
+ * INU Cafeteria is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * INU Cafeteria is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import express from 'express';
+import {logger} from '@inu-cafeteria/backend-core';
+import GetBookings from './GetBookings';
+import ConnectionPool from '../../../infrastructure/webserver/libs/ConnectionPool';
+
+class RealTimeBookingService {
+  private pool = new ConnectionPool();
+
+  /**
+   * 어느 사용자의 예약 내역 listener를 등록합니다.
+   *
+   * @param userId 사용자 식별자.
+   * @param res 응답 객체.
+   */
+  addBookingsListener(userId: number, res: express.Response) {
+    this.pool.add(`user_${userId}`, res);
+  }
+
+  /**
+   * 어느 사용자에게 예약 내역을 방출합니다.
+   * @param userId 사용자 식별자.
+   */
+  async emitBookings(userId: number) {
+    try {
+      const bookings = await GetBookings.run({userId});
+
+      await this.pool.broadcast(`user_${userId}`, 'bookings', bookings);
+    } catch (e) {
+      logger.error(`예약 내역 emit 실패: ${e}`);
+    }
+  }
+}
+
+export default new RealTimeBookingService();
