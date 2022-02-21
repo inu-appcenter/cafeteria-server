28ccd2fde859a869db2a2e9d2cc025734d269c7f
potados99 <potados99@gmail.com>
2022-02-22 01:06:54 +0900

    fix: 리팩토링하다 놓친 부분 해결

--- config.ts
+++ config.ts
@@ -155,8 +155,7 @@ export default {
 
     calender: {
       holidays: {
-        accountStatusUrl:
-          'https://calendar.google.com/calendar/ical/ko.south_korea.official%23holiday%40group.v.calendar.google.com/public/basic.ics',
+        url: 'https://calendar.google.com/calendar/ical/ko.south_korea.official%23holiday%40group.v.calendar.google.com/public/basic.ics',
       },
     },
   },
